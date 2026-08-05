320f04264a3785aef8162c93a888d7b5aaf56e0c
hirotomatsumoto <ma2hiro1127@icloud.com>
2026-08-06 00:15:53 +0900

    PC・iPhone同期、自動保存、モバイルUI改善

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'salon-manager-cache-v2';
+const CACHE_NAME = 'salon-manager-cache-v3';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/favicon.svg'];
 
 self.addEventListener('install', (event) => {
